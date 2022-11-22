05bc5871d736db0bc193f26858b01495950b14d0
addo <chokkadiadityarao@gmail.com>
2022-11-22 20:53:57 +0530

    add intitial width and height to grids and remove grid borders

--- javascript.js
+++ javascript.js
@@ -4,8 +4,8 @@ for(i=1;i<=256;i++) {
     grid.id="item"+i;
     grid.className="items";
     // grid.textContent="a";
-    grid.style.cssText="border:1px solid black; background:green;";
-    container.appendChild(grid);
+    grid.style.cssText="background:green; width:1px; height:31.3px;";// width is ignored because of flex-basis:0
+    container.appendChild(grid);                                      
     if(i%16==0) {
         let lineBreaker = document.createElement("div");
         lineBreaker.style.cssText="flex-basis:100%; height:0px;";
