6b05217c0913855e5d2f78a7d908b357c0a73941
addo <chokkadiadityarao@gmail.com>
2022-11-22 21:10:47 +0530

    add mouseover event on grids in order to change color to black

--- javascript.js
+++ javascript.js
@@ -1,15 +1,23 @@
+function colorGrid(e) {
+    this.style.cssText+="background:black;";
+}
+
 let container=document.querySelector("#container");
 for(i=1;i<=256;i++) {
     let grid=document.createElement("div");
     grid.id="item"+i;
     grid.className="items";
     // grid.textContent="a";
-    grid.style.cssText="background:green; width:1px; height:31.3px;";// width is ignored because of flex-basis:0
+    grid.style.cssText="width:1px; height:31.3px;";// width is ignored because of flex-basis:0
     container.appendChild(grid);                                      
     if(i%16==0) {
         let lineBreaker = document.createElement("div");
         lineBreaker.style.cssText="flex-basis:100%; height:0px;";
         container.appendChild(lineBreaker);
     }
-    
 }
+let grids=document.querySelectorAll(".items");
+grids.forEach((grid)=> {
+    grid.addEventListener("mouseover",colorGrid);
+});
+
