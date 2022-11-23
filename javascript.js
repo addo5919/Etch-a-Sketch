293238a47a0538dea0b0ea0798b0f7a6f75c2036
addo <chokkadiadityarao@gmail.com>
2022-11-23 21:48:29 +0530

    add draw mode

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 let color="black"; //this variable is used to assign color to div's during mouseover event
+let drawMode=true;
 
 function changeBoardSize(e) {
     let noOfColumns=Number(input.value);
@@ -25,18 +26,20 @@ function createBoard(noOfColumns) {                     //here no of columns = n
         // grid.textContent="a";
         grid.style.cssText="width:1px; height:31.3px;";// width is ignored because of flex-basis:0
         grid.addEventListener("mouseover",(e)=>{
-            if(color=="black") {
-              e.target.style.background="black";
-            }
-            else if(color=="random") {
-                let r=Math.floor(Math.random()*256);
-                let g=Math.floor(Math.random()*256);
-                let b=Math.floor(Math.random()*256); 
-                e.target.style.background=`rgb(${r},${g},${b})`;
-            }
-            else if(color=="erase") {
-                e.target.style.background="";
-            }
+            if(drawMode==true) {
+                if(color=="random") {
+                    let r=Math.floor(Math.random()*256);
+                    let g=Math.floor(Math.random()*256);
+                    let b=Math.floor(Math.random()*256); 
+                    e.target.style.background=`rgb(${r},${g},${b})`;
+                }
+                else if(color=="erase") {
+                    e.target.style.background="";
+                }
+                else {
+                e.target.style.background=color;
+                }
+            }  
         });
         container.appendChild(grid);                                      
         if(i%noOfColumns==0) {
@@ -48,6 +51,18 @@ function createBoard(noOfColumns) {                     //here no of columns = n
 }
 
 createBoard(16);
+let container=document.querySelector("#container");
+container.addEventListener("click",(e)=> {
+    let div=document.querySelector("#drawModeInfo"); 
+    if(drawMode){
+        drawMode=false;
+        div.textContent="drawMode is OFF! Click on board to turn ON.";
+    }
+    else {
+        drawMode=true;
+        div.textContent="drawMode is ON! Click on board to turn OFF.";
+    }
+ });
 
 let input=document.querySelector("input");
 let boardSizeBtn=document.querySelector("#boardSizeBtn");
